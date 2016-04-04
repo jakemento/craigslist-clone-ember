7307ac535196fbba5cb695c3f6138974f6b4a4e3
Jake Steinberg <jakesteinberg@gmail.com>
2016-04-04 13:35:25 -0700

    add listing model

---
+++ app/models/listing.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  image: DS.attr(),
+  text: DS.attr(),
+  title: DS.attr(),
+  address: DS.attr(),
+  zip: DS.attr(),
+  date: DS.attr(),
+  author: DS.attr()
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,8 @@ export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
       categories: this.store.findAll('category'),
-      subcategories: this.store.findAll('subcategory')
+      subcategories: this.store.findAll('subcategory'),
+      listings: this.store.findAll('listing')
     });
   },
 });
