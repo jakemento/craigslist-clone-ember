2a26808c84703629746e971bde482cae0e637eb8
Jake Steinberg <jakesteinberg@gmail.com>
2016-04-04 17:06:44 -0700

    add new functionality from index

--- app/components/category-detail.js
+++ app/components/category-detail.js
@@ -7,5 +7,20 @@ export default Ember.Component.extend({
       subcategories: this.store.findAll('subcategory'),
       listings: this.store.findAll('listing')
     });
+  },
+  actions: {
+    save2() {
+      var params = {
+        image: this.get('image'),
+        text: this.get('text'),
+        title: this.get('title'),
+        address: this.get('address'),
+        zip: this.get('zip'),
+        date: this.get('date'),
+        author: this.get ('author')
+      };
+      this.set('addNewListing', false);
+      this.sendAction('save3', params);
+    }
   }
 });
--- app/routes/category.js
+++ app/routes/category.js
@@ -4,4 +4,19 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('category', params.category_id);
   },
+  actions: {
+    save1() {
+      var params = {
+        image: this.get('image'),
+        text: this.get('text'),
+        title: this.get('title'),
+        address: this.get('address'),
+        zip: this.get('zip'),
+        date: this.get('date'),
+        author: this.get ('author')
+      };
+      this.set('addNewListing', false);
+      this.sendAction('save2', params);
+    }
+  }
 });
